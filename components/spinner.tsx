d85ee2550098cd6f89c053394a9c5ac68a6160dd
Tnaike <timmynaike@gmail.com>
2025-08-22 03:57:14 +0100

    App layout

---
+++ components/spinner.tsx
@@ -0,0 +1,36 @@
+import { VariantProps, cva } from 'class-variance-authority';
+import Image from 'next/image';
+
+export const spinnerVariants = cva('relative animate-spin', {
+  variants: {
+    size: {
+      small: 'w-6 h-6',
+      medium: 'w-10 h-10',
+      big: 'w-[9.75rem] h-[9.75rem]',
+    },
+    position: {
+      center: 'mx-auto',
+      default: '',
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+    position: 'default',
+  },
+});
+
+function Spinner({ size, position }: VariantProps<typeof spinnerVariants>) {
+  return (
+    <div className={spinnerVariants({ size, position })}>
+      <Image
+        width={80}
+        height={80}
+        alt="spinner"
+        src="/assets/loading-circle.svg"
+        className="object-fit"
+      />
+    </div>
+  );
+}
+
+export default Spinner;
